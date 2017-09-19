578bf774902479bb815e54aa60c0c6d6e8df4de6
Jonny Hork <JonnyHork@gmail.com>
2017-09-19 15:33:27 -0600

    all files complete

--- database_queries.sql
+++ database_queries.sql
@@ -6,4 +6,13 @@ SELECT "User".first_name,"User".last_name, contact.type, contact.value FROM "Use
 
 SELECT first_name FROM "User" WHERE id IN (1,3);
 
-SELECT u.first_name, COUNT(type) FROM "User" u JOIN contact c ON u.id = c.user_id GROUP BY first_name;
+SELECT u.first_name, COUNT(user_id) FROM "User" u JOIN contact c ON u.id = c.user_id GROUP BY first_name;
+
+ALTER TABLE "User"
+ADD is_human boolean;
+
+UPDATE "User" SET is_human = true WHERE id IN (1,2,4);
+
+DELETE FROM "User" WHERE is_human = true;
+
+DROP TABLE contact;
